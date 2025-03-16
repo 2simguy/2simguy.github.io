238cfcd51390e1b9984588f3eb804d6fcc622881
2simguy <42773827+2simguy@users.noreply.github.com>
2025-03-16 23:43:10 +0200

    getting started

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,18 @@
 import { Component } from '@angular/core';
-import { IonApp, IonRouterOutlet } from '@ionic/angular/standalone';
+import { IonApp, IonButton, IonIcon, IonRouterOutlet, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { addIcons } from 'ionicons';
+import { mailOutline, callOutline, logoGithub, logoLinkedin } from 'ionicons/icons';
 
 @Component({
   selector: 'app-root',
-  imports: [IonApp, IonRouterOutlet],
+  imports: [IonApp, IonRouterOutlet, IonToolbar, IonTitle, IonButton, IonIcon],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'CV';
+
+  constructor() {
+    addIcons({ mailOutline, callOutline, logoGithub, logoLinkedin });
+  }
 }
