0049e36d9f385a56920d78e19b299312c2a02900
2simguy <42773827+2simguy@users.noreply.github.com>
2025-03-16 22:38:32 +0200

    included ionic

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { IonApp, IonRouterOutlet } from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [IonApp, IonRouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'CV';
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,14 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { provideRouter, withPreloading, PreloadAllModules, RouteReuseStrategy } from '@angular/router';
 
 import { routes } from './app.routes';
+import { provideIonicAngular, IonicRouteStrategy } from '@ionic/angular/standalone';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes, withPreloading(PreloadAllModules)),
+    provideIonicAngular({}),
+    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
+  ],
 };
