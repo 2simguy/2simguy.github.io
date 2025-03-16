6c2f671dfa1f9969b028a3217ca979b66759edbd
2simguy <42773827+2simguy@users.noreply.github.com>
2025-03-16 23:09:06 +0200

    removed test code

--- CV/capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-import type { CapacitorConfig } from '@capacitor/cli';
-
-const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: 'CV',
-  webDir: 'www'
-};
-
-export default config;
